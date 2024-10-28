9bd095a7bfd475d3308624b462a6472bea316170
Comrade53 <legend8131@gmail.com>
2024-10-28 14:55:31 +0530

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+
+# fig = plt.figure()
+# plt.show()
+
+
+
+
+# 2
+# plt.plot([1, 2, 3, 4, 5], [1, 4, 5, 2, 1], color="red")
+# plt.xlim(1, 5)
+# plt.ylim(1, 6)
+
+# plt.xlabel("X-Axis")
+# plt.ylabel("Y-Axis")
+
+# plt.title("Demo Graph")
+# plt.legend(['values'])
+
+# plt.show()
+
+
+
+# 3
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# y = [1, 3, 5, 7, 6, 2, 6, 7, 5, 3]
+
+# plt.plot(x, y, color="red")
+# plt.show()
+
+# 4 Scatter
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# y = [1, 3, 5, 7, 6, 2, 6, 7, 5, 3]
+
+# plt.scatter(x, y, color="red")
+# plt.title("Scatter Chart")
+# plt.show()
+
+# 5 Pie
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# y = [1, 3, 5, 7, 6, 2, 6, 7, 5, 3]
+# plt.pie(x, y)
+# plt.show()
+
+# 6 Bar
+# x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# y = [1, 3, 5, 7, 6, 2, 6, 7, 5, 3]
+# plt.bar(x, y)
+# plt.show()
